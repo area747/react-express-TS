7f59eb876a4dc186250dcaa64d5fd71220b763d1
yusungho <yusungho@softin.co.kr>
2021-06-17 18:22:52 +0900

    - react file to client folder - 빌드 태스크 추가

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,13 @@ import e from 'express';
 import path from 'path';
 
 const app = e();
-//const root = path.join(__dirname,'..','client','build');
+app.use( '/', e.static( path.join( __dirname, '..', '..', 'client', 'build' ) ) );
+app.use( e.json() );
+app.use( e.urlencoded( { extended: true } ) )
 
+app.get( '/', function ( req, res, next ) {
+    res.sendFile( path.join( __dirname, '..', '..', 'client/build/index.html' ) );
+} );
 //app.use(e.static(root));
 
 app.listen( 7000 );
